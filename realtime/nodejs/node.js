2a14454628aad5de96f2f0752e25072e765b108d
omar <omarisel01@gmail.com>
2013-09-20 20:41:38 -0500

    server

---
+++ realtime/nodejs/node.js
@@ -0,0 +1,19 @@
+var app = require('http').createServer(),
+    io = require('socket.io').listen(app),
+    logger  = io.log,
+    redis = require('redis').createClient();
+
+var port = process.argv[2];
+app.listen(port);
+
+io.configure( function() {
+    io.set('close timeout', 60*60*24); // 24h time out
+});
+
+redis.psubscribe("channel_comments_*"); //subscribe by pattern
+
+io.sockets.on("connection", function(socket) {
+    redis.on("pmessage", function(pattern, channel, message) {
+        socket.emit(channel, message);
+    });
+});
